74f512a3cd93cf8cfd4af1b8ac254524493392ef
alexcherman <sashacherman@gmail.com>
2020-12-20 15:25:20 +0100

    Add tab switching logic

--- src/js/main.js
+++ src/js/main.js
@@ -1,31 +1,36 @@
 (function() {
-  var packagesButtons = document.querySelectorAll('.js-package-btn');
+  var buttonsList = document.querySelectorAll('.js-tab-btn')
 
-  function showSelectedPackage(package) {
-    document.querySelector('.installation__content.visible').classList.remove('visible');
-    document.getElementById(package + '-content').classList.add('visible');
+  function highlightSelectedButton(dataset) {
+    var selectedButtonGroup = document.querySelectorAll('[data-name=' + dataset.name + ']')
+    selectedButtonGroup.forEach(function(button) {
+      button.classList.remove('active');
+    })
+
+    var selectedButtons = document.querySelectorAll('[data-target=' + dataset.target + ']')
+    selectedButtons.forEach(function(button) {
+      button.classList.add('active');
+    })
+  }
+
+  function showSelectedContent(dataset) {
+    var selectedContent = document.querySelectorAll('[data-id=' + dataset.target + ']')
+    var selectedContentGroup = document.querySelectorAll('[data-group=' + dataset.name + ']')
+    
+    selectedContentGroup.forEach(function(content) {
+      content.classList.add('hidden');
+    })
+
+    selectedContent.forEach(function(content) {
+      content.classList.remove('hidden');
+    })
   }
   
-  function highlightSelectedPackageButton(button) {
-    document.querySelector('.js-package-btn.active').classList.remove('active');
-    button.classList.add('active');
-  }
-  
-  packagesButtons.forEach(function(button) {
+  buttonsList.forEach(function(button) {
     button.addEventListener('click', function() {
-      showSelectedPackage(button.dataset.target);
-      highlightSelectedPackageButton(button);
+      var dataset = button.dataset;
+      highlightSelectedButton(dataset)
+      showSelectedContent(dataset)
     })
-  });
-})();
-
-(function() {
-  var installationButton = document.querySelectorAll('.js-installation-btn');
-  
-  installationButton.forEach(function(button) {
-    button.addEventListener('click', function() {
-      console.log(button.dataset.target)
-    })
-  });
-
+  })
 })();
